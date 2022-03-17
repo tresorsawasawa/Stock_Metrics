42d046ee8e6e78a804c2febadc7da81370fb17b2
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-17 18:18:23 +0200

    Add logic in symbolsList componeent

--- src/components/Symbols/SymbolsList.jsx
+++ src/components/Symbols/SymbolsList.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { getSymbolsDispatcher } from '../../redux/companiesSymbols/companiesSymbols';
 import Symbol from './Symbol';
 
-const SymbolsList = () => (
-  <div>
-    <Symbol />
-  </div>
-);
+const SymbolsList = () => {
+  const dispatch = useDispatch();
+
+  const isSymbolsStored = useSelector(
+    (state) => state.companySymbolsReducer.isSymbolsStored || false,
+  );
+  const symbols = useSelector((state) => state.companySymbolsReducer.filteredSymbols || []);
+
+  useEffect(() => {});
+  if (!isSymbolsStored) {
+    dispatch(getSymbolsDispatcher);
+  }
+  return (
+    <div className="symbols-container">
+      {symbols.map((symbol) => (
+        <Symbol key={symbol.symbol} symbol={symbol.symbol} />
+      ))}
+      {symbols.length === 0 && isSymbolsStored && <div className="symbols-not-found">Not found</div>}
+    </div>
+  );
+};
 
 export default SymbolsList;
