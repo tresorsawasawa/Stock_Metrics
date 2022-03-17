d3b1e788b7c8d5971269577702f52f31a4d4c8f2
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-17 13:58:52 +0200

    UPdate APIs Infos

--- src/API/API.js
+++ src/API/API.js
@@ -1,8 +1,8 @@
-const API_KEY = '5f3cc5710d0dccc73af8b93c551f469a';
+const API_KEY = '?apikey=5f3cc5710d0dccc73af8b93c551f469a';
 const endpoint = 'https://financialmodelingprep.com/api/v3/';
-const Trading_Feature_Key = 'available-traded/list?apikey=';
+const Trading_Feature_Key = 'available-traded/list';
 const Profile_Feature = 'profile/';
 const FinancialModelingPrep_API_URL = `${endpoint}${Trading_Feature_Key}${API_KEY}`;
 const FinancialModelingPrep_CP_API_URL = `${endpoint}${Profile_Feature}`
 
-export { FinancialModelingPrep_API_URL, FinancialModelingPrep_CP_API_URL }
+export { API_KEY, FinancialModelingPrep_API_URL, FinancialModelingPrep_CP_API_URL }
