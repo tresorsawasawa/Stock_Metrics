85f5f5fca208d2c1d09e670780608fa88500c697
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-17 21:37:02 +0200

    Add the loader Gif and styling on it

---
+++ src/components/Spinner/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import spinner from '../../images/spinner.gif';
+
+const Spinner = () => (
+  <div>
+    <img src={spinner} alt="Loader Gif" />
+  </div>
+);
+
+export default Spinner;
