56c2e60a239a6b2475e6fea75ce6655cc2f1cb63
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-17 21:40:26 +0200

    import the the styling file of thr loader Gif

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import spinner from '../../images/spinner.gif';
+import './Spinner.scss';
 
 const Spinner = () => (
-  <div>
+  <div className="loader-img-container d-flex justify-content-center align-item-cnter">
     <img src={spinner} alt="Loader Gif" />
   </div>
 );
