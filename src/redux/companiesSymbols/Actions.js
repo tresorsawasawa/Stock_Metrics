2cd9810fed6642dfb72bd33e9920ddd5ff4bbe96
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-17 14:13:40 +0200

    Add redux Actions

---
+++ src/redux/companiesSymbols/Actions.js
@@ -0,0 +1,4 @@
+export const GET_SYMBOLS = 'companiesSymbols/Actions/GET_SYMBOLS';
+export const SEARCH_SYMBOLS = 'companiesSymbols/Actions/SEARCH_SYMBOLS';
+export const GET_COMPANY = 'companiesSymbols/Actions/GET_COMPANY';
+export const SHOW_LOADER = 'companiesSymbols/Actions/SHOW_LOADER';
