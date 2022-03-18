3aa555ee931a426d6bea6398c2d2813e14494ffe
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-18 17:02:53 +0200

    Add some classes to aloow styling

--- src/components/Symbols/Symbol.jsx
+++ src/components/Symbols/Symbol.jsx
@@ -3,23 +3,29 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { BsArrowRightCircle } from 'react-icons/bs';
 
-const Symbol = ({ symbol }) => (
-  <div className="symbol-container" data-testid={symbol.symbol}>
-    <div className="symbol-link">
-      <Link to={`/company/${symbol.symbol}`}>
-        <BsArrowRightCircle />
+const Symbol = ({
+  symbol, name, price, exchangeShortName,
+}) => (
+  <div className="symbol-container d-flex align-items-end flex-column">
+    <div className="symbol-link p-3" data-testid={symbol}>
+      <Link to={`/company/${symbol}`}>
+        <BsArrowRightCircle className="text-white fw-bolder fs-4" />
       </Link>
     </div>
-    <div className="symbol-info">
-      <span className="symbol-symbol">{symbol.symbol}</span>
-      <span className="symbol-name">{symbol.name}</span>
-      <span className="symbol-price">{symbol.price}</span>
+    <div className="symbol-info d-flex align-items-end flex-column pe-3 pb-2 text-end">
+      <span className="symbol-symbol fw-bolder fs-4">{symbol}</span>
+      <span className="symbol-name">{name}</span>
+      <span className="symbol-price fw-bolder fs-4 py-1">{price}</span>
+      <span className="symbol-exchShot">{exchangeShortName}</span>
     </div>
   </div>
 );
 
 Symbol.propTypes = {
-  symbol: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  symbol: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  exchangeShortName: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 };
 
 export default Symbol;
--- src/components/Symbols/SymbolsList.jsx
+++ src/components/Symbols/SymbolsList.jsx
@@ -10,19 +10,37 @@ const SymbolsList = () => {
   const isSymbolsStored = useSelector(
     (state) => state.companySymbolsReducer.isSymbolsStored || false,
   );
-  const symbols = useSelector((state) => state.companySymbolsReducer.filteredSymbols || []);
+  const symbols = useSelector(
+    (state) => state.companySymbolsReducer.filteredSymbols || [],
+  );
 
-  useEffect(() => {});
-  if (!isSymbolsStored) {
-    dispatch(getSymbolsDispatcher);
-  }
+  useEffect(() => {
+    if (!isSymbolsStored) {
+      dispatch(getSymbolsDispatcher());
+    }
+  });
+
   return (
-    <div className="symbols-container">
-      {symbols.map((symbol) => (
-        <Symbol key={symbol.symbol} symbol={symbol.symbol} />
-      ))}
-      {symbols.length === 0 && isSymbolsStored && <div className="symbols-not-found">Not found</div>}
-    </div>
+    <>
+      <div className="companies-list-title px-3 mt-4 py-2 d-flex flex-column justify-content-start align-items-start" />
+      <div className="symbols-container d-flex justify-content-center flex-wrap">
+        {symbols.map((symbol) => (
+          <Symbol
+            key={symbol.symbol}
+            symbol={symbol.symbol}
+            exchange={symbol.exchange}
+            exchangeShortName={symbol.exchangeShortName}
+            name={symbol.name}
+            price={symbol.price}
+          />
+        ))}
+        {symbols.length === 0 && isSymbolsStored && (
+          <span className="symbols-not-found">
+            Can&rsquo;t Find Any Symbols
+          </span>
+        )}
+      </div>
+    </>
   );
 };
 
