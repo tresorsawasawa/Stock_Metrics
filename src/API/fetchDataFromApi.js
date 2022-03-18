41e2b8a0e1190a291f5784a02b457d0915e4f9f0
tresorsawasawa <tresorsawasawa@gmail.com>
2022-03-18 19:03:25 +0200

    Remove unnessary code line

--- src/API/fetchDataFromApi.js
+++ src/API/fetchDataFromApi.js
@@ -5,8 +5,8 @@ const getSymbolsFromAPI = async () => {
   const getData = await axios.get(FMP_API_URL);
   const symbolsList = [];
 
-  for (let i = 0; i < 100; i += 1) {
-    symbolsList.push(getData([i]));
+  for (let i = 0; i < 1200; i += 1) {
+    symbolsList.push(getData.data[i]);
   }
 
   return symbolsList;
